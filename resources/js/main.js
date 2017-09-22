c2bcbe363ad05657c228a2614b399ac00323dc99
Salvador <salvav1@uci.edu>
2017-09-22 12:23:15 -0700

    Try to get carousel feature to work

--- resources/js/main.js
+++ resources/js/main.js
@@ -16,8 +16,14 @@ var scrollEvent = (function() {
   var currentVerticalPositionOfScrollBar = $(this).scrollTop();
   if (currentVerticalPositionOfScrollBar - howFarUserScroll > 50) {
       var heightOfNavbar = $('.navbar').css('height');
-      heightOfNavbar.animate()
+      $('.navbar').animate({top: '-' + heightOfNavbar}, 150);
+      howFarUserScroll = currentVerticalPositionOfScrollBar;
+  } else if (currentVerticalPositionOfScrollBar - howFarUserScroll < 50) {
+      $('.navbar').animate({top: '+' + 0}, 150);
+      howFarUserScroll = currentVerticalPositionOfScrollBar;
   }
 });
+
+
 
 $(window).scroll(scrollEvent);
