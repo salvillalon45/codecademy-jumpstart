57fec36fea8377535fc41d469d841fb5c8ec5db6
Salvador <salvav1@uci.edu>
2017-09-22 10:50:45 -0700

    Using Javascript

---
+++ resources/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".carousel").slick({
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: false;
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+
+});
