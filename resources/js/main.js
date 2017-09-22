ebaadbcaf4a9ba109ff08b6135f2463cc74fca98
Salvador <salvav1@uci.edu>
2017-09-22 11:49:50 -0700

    working on main.js

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $(".carousel").slick({
+  $('.carousel').slick({
     arrows: false,
     autoplay: true,
     autoplaySpeed: 4000,
@@ -9,5 +9,15 @@ $(document).ready(function() {
     pauseOnFocus: false,
     pauseOnHover: false
   });
-
 });
+
+var howFarUserScroll = 0;
+var scrollEvent = (function() {
+  var currentVerticalPositionOfScrollBar = $(this).scrollTop();
+  if (currentVerticalPositionOfScrollBar - howFarUserScroll > 50) {
+      var heightOfNavbar = $('.navbar').css('height');
+      heightOfNavbar.animate()
+  }
+});
+
+$(window).scroll(scrollEvent);
